5f12d5bd26e841199156cc8d33385238b8979db9
Adarsh <mamidiadarshreddy@gmail.com>
2026-07-27 22:10:25 +0530

    created react components

--- REACTJS/my-first-react-app/src/App.jsx
+++ REACTJS/my-first-react-app/src/App.jsx
@@ -1,11 +1,46 @@
-import Student from "./Student";
+import Student from "./components/student";
+import DOMTimer from "./components/domtimer";
 
 function App() {
+
+  const students=[
+    {
+      name:"Adarsh",
+      course:"CSE",
+      year:"2nd Year",
+      skill:"React"
+    },
+    {
+      name:"Priyanshu",
+      course:"CSE",
+      year:"2nd Year",
+      skill:"Python"
+    },
+    {
+      name:"Prudhvi",
+      course:"CSE",
+      year:"2nd Year",
+      skill:"Fullstack Developer"
+    },
+    {
+      name:"Pardhu",
+      course:"CSE",
+      year:"2nd Year",
+      skill:""
+    },
+    {
+      name:"Devi Prasad",
+      course:"CSE",
+      year:"2nd Year",
+      skill:"Java"
+    }
+  ]
+
   return (
     <div>
       <h1>College Portal</h1>
-
-      <Student />
+      <Student students={students}/>
+      <DOMTimer />
     </div>
   );
 }
